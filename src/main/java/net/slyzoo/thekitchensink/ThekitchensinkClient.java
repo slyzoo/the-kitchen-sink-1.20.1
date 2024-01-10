fd5bf433e99af89b95594a5edd71c16f8fedfc5b
slyzoo <gavinhimes@gmail.com>
2024-01-10 15:17:48 -0600

    added the mod lol

---
+++ src/main/java/net/slyzoo/thekitchensink/ThekitchensinkClient.java
@@ -0,0 +1,11 @@
+package net.slyzoo.thekitchensink;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThekitchensinkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
